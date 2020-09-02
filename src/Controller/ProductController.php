7a667e951d4697b5a13f3566a03c903364cdf06b
Bardiot <bardiot.marin@gmail.com>
2020-09-02 23:35:41 +0200

    add branch product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\ProductFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,12 +18,11 @@ class ProductController extends AbstractController
     /**
      * @Route("admin/add-product", name="add_product")
      */
-
 //    ici je cree ma methodes , de requette au formulaire , avec request qui me servira ici a upload
 //    Cet objet contient toutes les données possibles avec une requête http ( get , post , ect...)
 //    je cree mon C du CRUD
 
-    public function addProduct(Request $request): Response
+    public function addProduct(Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ProductFormType::class);
         $form->handleRequest($request);
@@ -35,7 +36,7 @@ class ProductController extends AbstractController
         }
 
 
-        return $this->render('shop.html.twig', [
+        return $this->render('product-form.html.twig', [
             "form_title" => "Ajoutez un produit",
             "form_product" => $form->createView(),
         ]);
