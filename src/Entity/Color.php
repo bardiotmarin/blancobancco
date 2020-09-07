436c17dc557890b2cfec06ed03b56a5c1bdc9dc3
Bardiot <bardiot.marin@gmail.com>
2020-09-08 00:22:16 +0200

    add font

---
+++ src/Entity/Color.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ColorRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ColorRepository::class)
+ */
+class Color
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
